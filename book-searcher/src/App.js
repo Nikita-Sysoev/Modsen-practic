b1f49f9d32d63325fae5167939174ab17a4c5562
Nikita Sysoev <555.nikita.sysoev.555@gmail.com>
2022-07-03 00:32:34 +0300

    Create a react app

--- book-searcher/src/App.js
+++ book-searcher/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      <div id="results-count">Found 5 results</div>
+
+      <div id="books">
+        <div class="book-card">
+          <img src="" alt="book image"/>
+          <div class="category">Category</div>
+          <div class="name">Name</div>
+          <div class="author">Author</div>
+        </div>
+      </div>
+
+    </>
   );
 }
 
